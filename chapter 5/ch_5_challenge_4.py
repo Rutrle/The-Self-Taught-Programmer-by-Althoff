145c2e44a4e52b0ee4d514b5d21494d87bb58427
Jakub <rutrle@seznam.cz>
2020-10-02 20:24:44 +0200

    fourth challenge of chapter 5

---
+++ chapter 5/ch_5_challenge_4.py
@@ -0,0 +1,9 @@
+
+person = {}
+
+person['height'] = input('Please enter your height: ')
+person['weight'] = input('Please enter your weight: ')
+person['favorite color'] = input('Please enter your favorite colour: ')
+
+
+print(person)
