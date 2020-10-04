1114ff8a998709eea98a3b95ce06d629de980fc7
Jakub <rutrle@seznam.cz>
2020-10-04 19:42:37 +0200

    second challenge of chapter 6

---
+++ chapter 6/ch_6_challenge_2.py
@@ -0,0 +1,8 @@
+
+input_1 = input('Please enter first string: ')
+input_2 = input('Please enter second string: ')
+
+
+print(f"Yesterday I wrote a {input_1}. I sent it to {input_2}!")
+
+print("Yesterday I wrote a {}. I sent it to {}!".format(input_1, input_2))
