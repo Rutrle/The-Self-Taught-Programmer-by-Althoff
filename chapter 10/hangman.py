eb2c56bfd40cd4a5c3d49812ed15dfb4bb95de07
Jakub <rutrle@seznam.cz>
2020-10-07 22:59:59 +0200

    Hangman function definition

---
+++ chapter 10/hangman.py
@@ -0,0 +1,10 @@
+
+
+def hangman(word):
+    wrong = 0
+    stages = ["", "________      ", "|      |      ",
+              "|      0      ", "|     /|\     ", "|     / \     ", "|"]
+    rletters = list(word)
+    board = ["__"]*len(word)
+    win = False
+    print("welcome to hangman")
