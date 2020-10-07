dfb323a2d9fb43f09cf12d9c233714ae0fdb43ce
Jakub <rutrle@seznam.cz>
2020-10-07 23:58:14 +0200

    Hangman loop part

--- chapter 10/hangman.py
+++ chapter 10/hangman.py
@@ -8,3 +8,23 @@ def hangman(word):
     board = ["__"]*len(word)
     win = False
     print("welcome to hangman")
+
+
+while wrong < len(stages)-1:
+    print("\n")
+    msg = "Guess a letter"
+    if char in rletters:
+        cind = rletters.index(char)
+        board[cind] = char
+        rletters[cind] = '$'
+
+    else:
+        wrong += 1
+    print((" ".join(board)))
+    e = wrong + 1
+    print("\n".join(stages[0:e]))
+    if "__" not in board:
+        print("You win!")
+        print(" ".join(board))
+        win = True
+        break
