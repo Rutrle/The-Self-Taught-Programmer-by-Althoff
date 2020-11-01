e98ba2aa482f55f4fb6e8b55635e96d60f0e2bdf
Jakub <rutrle@seznam.cz>
2020-11-01 23:33:00 +0100

    finding RE at start and end of lines

--- chapter_17/regular_expression.py
+++ chapter_17/regular_expression.py
@@ -10,3 +10,15 @@ print(matches)
 matches_insensitive = re.findall('beautiful', my_list, re.IGNORECASE)
 
 print(matches_insensitive)
+
+
+zen = """Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!"""
+
+m = re.findall("^If", zen, re.MULTILINE)
+print(m)
+
+m = re.findall("idea.$", zen, re.MULTILINE)
+print(m)
