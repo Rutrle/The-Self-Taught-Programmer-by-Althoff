f84c795b2093dc7ec0445b9a7edbdfb4611ac792
Jakub <rutrle@seznam.cz>
2020-11-11 00:45:07 +0100

    finished webscraper

--- chapter_20/web_scraper.py
+++ chapter_20/web_scraper.py
@@ -7,8 +7,21 @@ class Scraper:
     def __init__(self, site):
         self.site = site
 
-        def scrape(self):
-            r = urllib.request.urlopen(self.site)
-            html = r.read()
-            parser = "html.parser"
-            sp = BeautifulSoup(html, parser)
+    def scrape(self):
+        r = urllib.request.urlopen(self.site)
+        html = r.read()
+        parser = "html.parser"
+        sp = BeautifulSoup(html, parser)
+        for tag in sp.find_all("a"):
+
+            url = tag.get("href")
+            # print(url)
+
+            if url is None:
+                continue
+            if "html" in url:
+                print("\n"+url)
+
+
+news = "https://www.novinky.cz/"
+Scraper(news).scrape()
